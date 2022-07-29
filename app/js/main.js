8b6862ae3ab7544eb25354b8dcf74e62753eb827
triplemonster221 <Boorder1993@yandex.ru>
2022-07-29 21:30:55 +0500

    added type

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 $('.slick-slider').slick({
   centerMode: true,
   slidesToShow: 3,
+  centerPadding:'20px',
   dots: true,
   arrows: true,
   swipe: true,
@@ -32,6 +33,22 @@ $('.slick-slider').slick({
     //   }
     ]
   });
+
+
+  $('.shop-slider').slick({
+    slidesToShow: 4,
+    dots: false,
+    arrows: true,
+    swipe: true,
+  })
+
+  $('.types-slider').slick({
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    dots: false,
+    arrows: true,
+    swipe: true,
+  })
 
 
 
