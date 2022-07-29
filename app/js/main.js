5f5ee9c488cec51a08f251342a290238d6e09533
triplemonster221 <Boorder1993@yandex.ru>
2022-07-29 11:22:36 +0500

    added asortiment

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,38 @@
-console.log('111');
+
+$(document).ready(function(){
+    
+ 
+$('.slick-slider').slick({
+  centerMode: true,
+  slidesToShow: 3,
+  dots: true,
+  arrows: true,
+  swipe: true,
+//  infinite: true,
+  swipeToSlide: true,
+  adaptiveHeight: true,
+    responsive: [
+    //   {
+    //     breakpoint: 768,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 3
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 1
+    //     }
+    //   }
+    ]
+  });
+
+
+
+});
